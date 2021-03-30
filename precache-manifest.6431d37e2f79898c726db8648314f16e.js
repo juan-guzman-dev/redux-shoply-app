473476c985f566a0c4b076425d97dc2dbb94c194
Juan Guzman <juan.guzman.sd@gmail>
2021-03-30 09:31:14 -0500

    Updates

---
+++ precache-manifest.6431d37e2f79898c726db8648314f16e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed57f383b1e13cf3db5ec105918cd7e0",
+    "url": "/redux-shoply-app/index.html"
+  },
+  {
+    "revision": "38d195b65180c10157f8",
+    "url": "/redux-shoply-app/static/css/2.4c177f38.chunk.css"
+  },
+  {
+    "revision": "31cf4bceb9765ee4cbfc",
+    "url": "/redux-shoply-app/static/css/main.a8473187.chunk.css"
+  },
+  {
+    "revision": "38d195b65180c10157f8",
+    "url": "/redux-shoply-app/static/js/2.0628645c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/redux-shoply-app/static/js/2.0628645c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31cf4bceb9765ee4cbfc",
+    "url": "/redux-shoply-app/static/js/main.ad91233f.chunk.js"
+  },
+  {
+    "revision": "62c49f38bddcda40de94",
+    "url": "/redux-shoply-app/static/js/runtime-main.1793954d.js"
+  },
+  {
+    "revision": "099a9556e1a63ece24f8a99859c94c7d",
+    "url": "/redux-shoply-app/static/media/fa-brands-400.099a9556.woff"
+  },
+  {
+    "revision": "30cc681d4487d2f561035ba24a68c629",
+    "url": "/redux-shoply-app/static/media/fa-brands-400.30cc681d.eot"
+  },
+  {
+    "revision": "3b89dd103490708d19a95adcae52210e",
+    "url": "/redux-shoply-app/static/media/fa-brands-400.3b89dd10.ttf"
+  },
+  {
+    "revision": "ba7ed552362f64d30f6d844974d89114",
+    "url": "/redux-shoply-app/static/media/fa-brands-400.ba7ed552.svg"
+  },
+  {
+    "revision": "f7307680c7fe85959f3ecf122493ea7d",
+    "url": "/redux-shoply-app/static/media/fa-brands-400.f7307680.woff2"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/redux-shoply-app/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/redux-shoply-app/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/redux-shoply-app/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/redux-shoply-app/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/redux-shoply-app/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/redux-shoply-app/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/redux-shoply-app/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/redux-shoply-app/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/redux-shoply-app/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/redux-shoply-app/static/media/fa-solid-900.e8a427e1.woff2"
+  }
+]);
